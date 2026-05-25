0eb941cc82122f9d1e7529def9c567d973ea5031
Annaeil <Ankjeil@icloud.com>
2026-05-25 17:26:56 +0200

    ferdigstillt oppskrift med js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-    const burgerKnapp = document.querySelector(".burger-knapp");
-    const navbar = document.querySelector(".navbar");
+const burgerKnapp = document.querySelector(".burger-knapp");
+const navbar = document.querySelector(".navbar");
 
-    burgerKnapp.addEventListener("click", function () {
-        navbar.classList.toggle("active");
+burgerKnapp.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+const oppskriftPunkter = document.querySelectorAll(".sjekkliste li, .stegliste li");
+
+oppskriftPunkter.forEach(function (punkt) {
+    punkt.addEventListener("click", function () {
+        punkt.classList.toggle("ferdig");
     });
+});
